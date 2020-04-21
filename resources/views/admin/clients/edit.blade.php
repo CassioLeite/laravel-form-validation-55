ba5fc20fb854e733d705f856ce234f3390e5d8b5
CassioLeite <cassiodegoes@live.com>
2020-04-21 16:58:35 -0300

    Recuperando dados digitados em formularios - Route Model Binding

--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.layout')
-
 @section('content')
     <h3>Novo Cliente</h3>
     
     @include('admin.form._form_errors')
         
-    <form method="post" action="{{ route('clients.update', ['id' => $client->id]) }}">
+    <form method="post" action="{{ route('clients.update', ['client' => $client->id]) }}">
         {{ method_field('PUT') }}
         @include('admin.clients._form')
         <button type="submit" class="btn btn-default">Salvar</button>
--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -28,7 +28,7 @@
                     <td>{{ $client->phone }}</td>
                     <td>{{ $client->sex }}</td>
                     <td>
-                        <a href="{{ route('clients.edit', ['id' => $client->id ]) }}">Editar</a>
+                        <a href="{{ route('clients.edit', ['client' => $client->id ]) }}">Editar</a>
                     </td>
                 </tr>
             @endforeach
