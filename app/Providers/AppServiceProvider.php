f00447c6bac0abcb8eb2b24e4b51dd3424de6452
CassioLeite <cassiodegoes@live.com>
2020-04-23 00:15:48 -0300

    Validaçao de unicidade do CPF e CNPJ - Validaçao do codigo verificador de CPF e CNPJ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Code\Validator\Cnpj;
+use Code\Validator\Cpf;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
     {
         $platform = \Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform(); 
         $platform->registerDoctrineTypeMapping('enum', 'string');
+
+        \Validator::extend('document_number', function ($attribute, $value, $parameters, $validator) {
+            $documentValidator = $parameters[0] == 'cpf' ? new Cpf() : new Cnpj();
+            return $documentValidator->isValid($value);
+        });
     }
 
     /**
